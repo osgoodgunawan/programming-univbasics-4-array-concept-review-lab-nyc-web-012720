48d32605430cfb8c71dcc735350afa2c24b4516b
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-07 19:28:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
   counter=0
   while counter<array.size do 
-    
+    if array[counter]==value_to_find  do 
+      
     counter+=1
   end
-  
 end
 
 def find_max_value(array)
