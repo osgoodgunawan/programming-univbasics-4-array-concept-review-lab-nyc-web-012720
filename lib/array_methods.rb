25c2c6bdfd0ad12d78b9ab00179853f0f41efd6d
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-07 19:38:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   counter=0
   while counter<array.size do 
-    if array[counter]==value_to_find  do 
+    if array[counter]==value_to_find 
       return counter
     counter+=1
   end
